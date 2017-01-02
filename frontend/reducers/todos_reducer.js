8d3c09720a71c2c036911f2811ef8ccc8e21a933
PrestonPhelan <preston.phelan@gmail.com>
2017-01-02 12:16:01 -0800

    Store created

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,9 +1,15 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+import merge from 'lodash/merge';
 
 const todosReducer = (state = {}, action) => {
+  Object.freeze(state);
   switch(action.type) {
     case RECEIVE_TODOS:
       return action.todos;
+    case RECEIVE_TODO:
+      let newState = merge({}, state);
+      newState[action.todo.id] = action.todo;
+      return newState;
     default:
       return state;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import configureStore from './store/store.js';
+
+window.store = configureStore();
 
 const App = () => (
   // <Provider store={store}
